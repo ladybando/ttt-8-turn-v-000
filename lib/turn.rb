e2d9f3795a3b0cc1a75579b141b4a705f8c0f2ff
Ayana Bando <ayanaflax@yahoo.com>
2017-10-26 17:12:57 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,9 @@ def display_board(board)
    puts "-----------"
    puts " #{board[6]} | #{board[7]} | #{board[8]} "
  end
+ def input_to_index(user_input)
+    user_input.to_i - 1
+end
 
 
 # code your #valid_move? method here
@@ -28,4 +31,5 @@ end
 #code turn method here
 def turn(board)
   puts "Please enter 1-9:"
+  if 
 end
