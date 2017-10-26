32dfc0609f61d23be27c74c5a3a17badd048e59b
Ayana Bando <ayanaflax@yahoo.com>
2017-10-26 16:51:08 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,13 @@
+#code display board here
+def display_board(board)
+   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+   puts "-----------"
+   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+   puts "-----------"
+   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+ end
+
+
 # code your #valid_move? method here
 def valid_move?(board, index)
   if position_taken?(board, index) == true
